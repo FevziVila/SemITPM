b92f47703b5a4da0fa592e26b469edd8ac5e5a3e
fevzi <fevzi.vila@dpschool.io>
2024-12-10 15:43:05 +0100

    test

--- frontend.gitops/gitops/src/App.test.js
+++ frontend.gitops/gitops/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders search interface', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  const titleElement = screen.getByText(/search tickers/i);
+  const searchInput = screen.getByPlaceholderText(/enter search term/i);
+  const searchButton = screen.getByText(/search/i);
+  const resultsHeading = screen.getByText(/results:/i);
+  
+  expect(titleElement).toBeInTheDocument();
+  expect(searchInput).toBeInTheDocument();
+  expect(searchButton).toBeInTheDocument();
+  expect(resultsHeading).toBeInTheDocument();
 });
