7b42ce58ea20406fffcd079b880582392ef0e475
fevzi <fevzi.vila@dpschool.io>
2024-12-10 16:56:13 +0100

    delete unused code

--- backend.gitops/src/main/java/backend/gitops/controller/Controller.java
+++ backend.gitops/src/main/java/backend/gitops/controller/Controller.java
@@ -6,12 +6,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.PostMapping;
 
 
 @RestController
@@ -21,12 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class Controller {
 
     private final Service service;
-    
-    @GetMapping("test")
-    public String returnString() {
-        return "Hello";
-    }
-
     
     @GetMapping("search")
     public ResponseEntity<?> getTickers(@RequestParam String args) {
